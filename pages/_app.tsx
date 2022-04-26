3cbed7b722ad819e7b2c0992add3b58b5e0b8baa
aslampangestu <aslampangestu@gmail.com>
2022-04-26 15:22:15 +0700

    sign up - convert html

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import 'styles/detail.css';
 import 'styles/checkout.css';
 import 'styles/complete-checkout.css';
 import 'styles/sign-in.css';
+import 'styles/sign-up.css';
 
 import { useEffect } from 'react';
 import AOS from 'aos';
